c8a1a1cff4654261e323bfb154330f1baa4cf935
MounaKossentini <60269332+MounaKossentini@users.noreply.github.com>
2021-10-31 18:32:38 +0100

    service_entreprise_v1

--- src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
@@ -3,6 +3,8 @@ package tn.esprit.spring.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,11 +17,14 @@ import tn.esprit.spring.repository.EntrepriseRepository;
 
 @Service
 public class EntrepriseServiceImpl implements IEntrepriseService {
+	private static final Logger logger = LogManager.getLogger(EmployeServiceImpl.class);
 
 	@Autowired
     EntrepriseRepository entrepriseRepoistory;
 	@Autowired
 	DepartementRepository deptRepoistory;
+	
+	
 	@Override
 	public int ajouterEntreprise(Entreprise entreprise) {
 		//oui
@@ -27,8 +32,17 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 	}
 	@Override
 	public int ajouterDepartement(Departement dep) {
-		//oui
-		return 0;
+		try{
+		deptRepoistory.save(dep);
+		logger.info(" departement ajouté)");
+    	logger.debug("ajout departement)");
+		}
+	catch (Exception e) {
+		 logger.error("erreur ajout departement");
+	}
+	
+		return dep.getId();
+
 	}
 	@Override
 	public void affecterDepartementAEntreprise(int depId, int entrepriseId) {
@@ -37,18 +51,38 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 	}
 	@Override
 	public List<String> getAllDepartementsNamesByEntreprise(int entrepriseId) {
-		//oui
-		return null;
+		List<String> depNames = new ArrayList<>();
+		try{
+		Entreprise entrepriseManagedEntity = this.entrepriseRepoistory.findById(entrepriseId).get();
+		for(Departement dep : entrepriseManagedEntity.getDepartements()){
+			depNames.add(dep.getName());
+			logger.info(" liste de tout les departements par entreprise affiché)");
+	    	logger.debug("tout les departements par entreprise)");
+			
+		}
+		}
+		catch (Exception e) {
+			 logger.error(" liste de tout les departements par entreprise failed");
+		}
+		
+		return depNames;
 	}
 	@Override
 	public void deleteEntrepriseById(int entrepriseId) {
 		//oui
 		
 	}
-	@Override
-	public void deleteDepartementById(int depId) {
-		//oui
-		
+	@Transactional
+	public int deleteDepartementById(int depId) {
+		try{
+		this.deptRepoistory.delete(this.deptRepoistory.findById(depId).get());
+		logger.info(" departement suprimé avec succés )");
+    	logger.debug("suppression departement)");
+		}
+		catch (Exception e) {
+			 logger.error(" suppression departement failed");
+		}
+		return 0;		
 	}
 	@Override
 	public Entreprise getEntrepriseById(int entrepriseId) {
--- src/main/java/tn/esprit/spring/services/IEntrepriseService.java
+++ src/main/java/tn/esprit/spring/services/IEntrepriseService.java
@@ -12,6 +12,6 @@ public interface IEntrepriseService {
 	void affecterDepartementAEntreprise(int depId, int entrepriseId);
 	List<String> getAllDepartementsNamesByEntreprise(int entrepriseId);
 	public void deleteEntrepriseById(int entrepriseId);
-	public void deleteDepartementById(int depId);
+	public int deleteDepartementById(int depId);
 	public Entreprise getEntrepriseById(int entrepriseId);
 }
